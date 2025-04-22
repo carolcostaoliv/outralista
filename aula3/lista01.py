b5dd531795750b36566b1cd8337274c959c5ad6b
carolcostaoliv <carolinaoliveiraacosta@gmail.com>
2025-04-22 13:31:04 +0000

    começo da lista 01

--- aula3/lista01.py
+++ aula3/lista01.py
@@ -1,1 +1,22 @@
+#01
+
+a = float(input())
+b = float(input())
+if a > b:
+    print (a)
+if b > a:
+    print (b)
+else:
+    print ("Números iguais")
+
+#2
+
+nota1 = float(input())
+nota2 = float(input())
+nota3 = float(input())
+nota4 = float(input())
+
+media = (nota1 + nota2 + nota3 = nota4)/4
+
+print(f"Mádia = {media}")
 
